4764130cf52ba6df0f5e36836c6588d111c24fc6
Antti S. Lankila <alankila@bel.fi>
2014-10-14 17:37:30 +0300

    Fix various warnings, and compilation issues
    Also made the hash entry key handling to work. I don't really
    understand how Perl is put together and why these SVs don't
    seem to know if they are UTF-8 encoded or not. Did anyone really
    even try to get this working properly, ever?

--- V8Context.cpp
+++ V8Context.cpp
@@ -262,7 +262,7 @@ void PerlObjectData::add_size(size_t bytes_) {
 Handle<Value>
 PerlFunctionData::invoke(const Arguments& args) {
     SETUP_PERL_CALL();
-    int count = call_sv(rv, G_SCALAR | G_EVAL);
+    (void) call_sv(rv, G_SCALAR | G_EVAL);
     CONVERT_PERL_RESULT();
 }
 
@@ -282,7 +282,7 @@ public:
 Handle<Value>
 PerlMethodData::invoke(const Arguments& args) {
     SETUP_PERL_CALL(mXPUSHs(context->v82sv(args.This())))
-    int count = call_method(name.c_str(), G_SCALAR | G_EVAL);
+    (void) call_method(name.c_str(), G_SCALAR | G_EVAL);
     CONVERT_PERL_RESULT()
 }
 
@@ -520,7 +520,7 @@ V8Context::v82sv(Handle<Value> value, SvMap& seen) {
         return newSV(0);
 
     if (value->IsNull())
-        return newSv(0);
+        return newSV(0);
 
     if (value->IsInt32())
         return newSViv(value->Int32Value());
@@ -576,7 +576,7 @@ V8Context::v82sv(Handle<Value> value) {
 void
 V8Context::fill_prototype(Handle<Object> prototype, HV* stash) {
     HE *he;
-    while (he = hv_iternext(stash)) {
+    while ((he = hv_iternext(stash))) {
         SV *key = HeSVKEY_force(he);
         Local<String> name = String::New(SvPV_nolen(key));
 
@@ -680,17 +680,27 @@ V8Context::av2array(AV *av, HandleMap& seen, long ptr) {
 
 Handle<Object>
 V8Context::hv2object(HV *hv, HandleMap& seen, long ptr) {
-    HE *he;
-    SV *key;
-    SV *value;
+    HE* he;
+    SV* key;
+    SV* value;
+    STRLEN keylen;
+    const char* keyval;
 
     hv_iterinit(hv);
     Handle<Object> object = Object::New();
     seen[ptr] = object;
-    while (he = hv_iternext(hv)) {
-	key = HeSVKEY_force(he);
+    while ((he = hv_iternext(hv))) {
 	value = HeVAL(he);
-        object->Set(sv2v8str(key), sv2v8(val, seen));
+        if (HeUTF8(he)) {
+            /* Apparently HeSVKEY returns the hash key scalar without the
+             * UTF-8 flag enabled. Asking the UTF-8 bytes in sv2v8str causes
+             * double-encoded UTF-8 to be seen by v8. */
+            keyval = HePV(he, keylen);
+            object->Set(String::New(keyval, keylen), sv2v8(value, seen));
+        } else {
+            key = HeSVKEY_force(he);
+            object->Set(sv2v8str(key), sv2v8(value, seen));
+        }
     }
     return object;
 }
@@ -708,8 +718,8 @@ V8Context::array2sv(Handle<Array> array, SvMap& seen) {
 
     seen.add(array, PTR2IV(av));
 
-    for (int i = 0; i < array->Length(); i++) {
-        Handle<Value> elementVal = array->Get( Integer::New( i ) );
+    for (uint32_t i = 0; i < array->Length(); i++) {
+        Handle<Value> elementVal = array->Get( Integer::NewFromUnsigned( i ) );
         av_push(av, v82sv(elementVal, seen));
     }
     return rv;
@@ -728,8 +738,8 @@ V8Context::object2sv(Handle<Object> obj, SvMap& seen) {
     seen.add(obj, PTR2IV(hv));
 
     Local<Array> properties = obj->GetPropertyNames();
-    for (int i = 0; i < properties->Length(); i++) {
-        Local<Integer> propertyIndex = Integer::New( i );
+    for (uint32_t i = 0; i < properties->Length(); i++) {
+        Local<Integer> propertyIndex = Integer::NewFromUnsigned( i );
         Local<String> propertyName = Local<String>::Cast( properties->Get( propertyIndex ) );
         String::Utf8Value propertyNameUTF8( propertyName );
 
@@ -831,7 +841,7 @@ XS(v8method) {
 SV*
 V8Context::function2sv(Handle<Function> fn) {
     CV          *code = newXS(NULL, v8closure, __FILE__);
-    V8ObjectData *data = new V8FunctionData(this, fn->ToObject(), (SV*)code);
+    (void) new V8FunctionData(this, fn->ToObject(), (SV*)code);
     return newRV_noinc((SV*)code);
 }
 
@@ -856,7 +866,7 @@ V8Context::object2blessed(Handle<Object> obj) {
         stash = gv_stashpv(package, GV_ADD);
 
         Local<Array> properties = prototype->GetPropertyNames();
-        for (int i = 0; i < properties->Length(); i++) {
+        for (uint32_t i = 0; i < properties->Length(); i++) {
             Local<String> name = properties->Get(i)->ToString();
             Local<Value> property = prototype->Get(name);
 
@@ -866,7 +876,7 @@ V8Context::object2blessed(Handle<Object> obj) {
             Local<Function> fn = Local<Function>::Cast(property);
 
             CV *code = newXS(NULL, v8method, __FILE__);
-            V8ObjectData *data = new V8FunctionData(this, fn, (SV*)code);
+            (void) new V8FunctionData(this, fn, (SV*)code);
 
             GV* gv = (GV*)*hv_fetch(stash, *String::AsciiValue(name), name->Length(), TRUE);
             gv_init(gv, stash, *String::AsciiValue(name), name->Length(), GV_ADDMULTI); /* vivify */
